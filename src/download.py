8aef4cd1e6972222842567ef78cea95557ff1aa0
sarmengol777 <sarmengol777@gmail.com>
2026-02-09 10:57:22 +0100

    commit sanas

--- src/download.py
+++ src/download.py
@@ -13,4 +13,14 @@ headers = {
 
 response = requests.get(url_popular_movies, headers=headers)
 
-print(response.json()["results"])
+os.makedirs("data/raw", exist_ok=True)
+
+#Guardar el archivo
+file_path = "data/raw/popular_movies.json"
+
+with open(file_path, "w", encoding="utf-8") as f:
+    for movie in movie:
+        f.write (json.dudmps(movie, esure_ascii=false) +"/n")
+   ## json.dump(movies, f, indent=4, ensure_ascii=False)
+
+print(f"Se guardaron {len(movies)} películas en {file_path}")
