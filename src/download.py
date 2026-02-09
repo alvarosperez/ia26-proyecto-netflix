46fa5c59c372005ab6512aa8c3cc30f3e6403524
Yesesesesesssssssss <166861002+Yesesesesesssssssss@users.noreply.github.com>
2026-02-09 11:38:14 +0100

    Géneros y Películas v1

--- src/download.py
+++ src/download.py
@@ -1,27 +1,36 @@
-
 import requests
 import json 
 import os 
 from config import ACCESS_TOKEN
 
+# SECCIÓN DE INFORMACIÓN Y VARIABLES DE PELÍCULAS POPULARES:
 url_popular_movies = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
 headers = {
     "accept": "application/json",
     "Authorization": f"Bearer {ACCESS_TOKEN}"
 }
+response_movies = requests.get(url_popular_movies, headers=headers)
+movies = response_movies.json()["results"]
 
-response = requests.get(url_popular_movies, headers=headers)
-movies = response.json()["results"]
 
+url_genre_movies = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+response_genres = requests.get(url_genre_movies, headers=headers)
+genres = response_genres.json()["genres"]
+
+
+# CÓDIGO IMPRIMIR PELÍCULAS POPULARES Y GÉNEROS:
 os.makedirs("data/raw", exist_ok=True)
 
-#Guardar el archivo
-file_path = "data/raw/popular_movies.json"
+file_path_movies = "data/raw/popular_movies.json"
 
-with open(file_path, "w", encoding="utf-8") as f:
+with open(file_path_movies, "w", encoding="utf-8") as f:
     for movie in movies:
         f.write(json.dumps(movie) + "\n")
 
+file_path_genres = "data/raw/genre_movies.json"
+with open(file_path_genres, "w", encoding = "utf-8") as a:
+    for genre in genres:
+        a.write(json.dumps(genre) + "\n")
 
-print(f"Se guardaron {len(movies)} películas en {file_path}")
+print(f"Se guardaron {len(movies)} películas en {file_path_movies} y {len(genres)} géneros en {file_path_genres}")
 
