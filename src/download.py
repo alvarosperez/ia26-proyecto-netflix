753f3f64eefadf30169f2c15716b76ce1b01d80a
mateomoliina <moligari55@gmail.com>
2026-02-09 11:37:37 +0100

    genres

--- src/download.py
+++ src/download.py
@@ -5,11 +5,14 @@ import requests
 from config import ACCESS_TOKEN
 
 url_popular_movies = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+url_genres = "https://api.themoviedb.org/3/genre/movie/list?language=en"
 
 headers = {
     "accept": "application/json",
     "Authorization": f"Bearer {ACCESS_TOKEN}"
 }
+
+##Listado Peliculas
 
 response = requests.get(url_popular_movies, headers=headers)
 movies = response.json()["results"]
@@ -24,3 +27,16 @@ with open(file_path, "w", encoding="utf-8") as f:
         f.write(json.dumps(movie) + "\n")
 
 print(f"Se guardaron {len(movies)} películas en {file_path}")
+
+##Generos
+
+response_genre = requests.get(url_genres, headers = headers)
+genres = response_genre.json()["genres"]
+
+genres_path = "data/raw/genres.json"
+
+with open(genres_path, "w", encoding="utf-8") as f:
+    for genre in genres:
+        f.write(json.dumps(genre) + "\n")
+
+print(f"Se guardaron {len(genres)} películas en {genres_path}")
