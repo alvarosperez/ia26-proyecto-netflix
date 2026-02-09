fb7fff646e063db1749a35d67ce79f0bdd56b8e7
mateomoliina <moligari55@gmail.com>
2026-02-09 12:14:52 +0100

    funciones

--- src/download.py
+++ src/download.py
@@ -4,39 +4,34 @@ import requests
 
 from config import ACCESS_TOKEN
 
-url_popular_movies = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+def api_request(url):
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {ACCESS_TOKEN}"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+def data_writing(file_path, data):
+   
+    os.makedirs("data/raw", exist_ok=True)
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        for element in data:
+            f.write(json.dumps(element) + "\n")
+
+    print(f"Se guardaron {len(data)} películas en {file_path}")
+
+
+##Peliculas
+url_movies = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+movie_data = api_request(url_movies)
+movie_path = "data/raw/popular_movies.json"
+data_writing(movie_path, movie_data["results"]) 
+
+##Genres
 url_genres = "https://api.themoviedb.org/3/genre/movie/list?language=en"
-
-headers = {
-    "accept": "application/json",
-    "Authorization": f"Bearer {ACCESS_TOKEN}"
-}
-
-##Listado Peliculas
-
-response = requests.get(url_popular_movies, headers=headers)
-movies = response.json()["results"]
-
-os.makedirs("data/raw", exist_ok=True)
-
-#Guardar el archivo
-file_path = "data/raw/popular_movies.json"
-
-with open(file_path, "w", encoding="utf-8") as f:
-    for movie in movies:
-        f.write(json.dumps(movie) + "\n")
-
-print(f"Se guardaron {len(movies)} películas en {file_path}")
-
-##Generos
-
-response_genre = requests.get(url_genres, headers = headers)
-genres = response_genre.json()["genres"]
-
+genre_data = api_request(url_genres)
 genres_path = "data/raw/genres.json"
-
-with open(genres_path, "w", encoding="utf-8") as f:
-    for genre in genres:
-        f.write(json.dumps(genre) + "\n")
-
-print(f"Se guardaron {len(genres)} películas en {genres_path}")
+data_writing(genres_path, genre_data["genres"])   
