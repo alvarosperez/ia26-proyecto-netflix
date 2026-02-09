195f03198fcf71fd9c8ceef430e30798cd3558df
adrianreigfernandez-oss <reigfernandezadrian@gmail.com>
2026-02-09 11:37:39 +0100

    archivos

--- src/download.py
+++ src/download.py
@@ -5,6 +5,7 @@ import requests
 from config import ACCESS_TOKEN
 
 url_popular_movies = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+url_movie_list = "https://api.themoviedb.org/3/genre/movie/list"
 
 headers = {
     "accept": "application/json",
@@ -22,6 +23,20 @@ os.makedirs("data/raw", exist_ok=True)
 file_path = "data/raw/popular_movies.json"
 
 with open(file_path, "w", encoding="utf-8") as f:
-    json.dump(movies, f, indent=4, ensure_ascii=False)
+    for movie in movies:
+        f.write(json.dumps(movie) + "\n")
 
 print(f"Se guardaron {len(movies)} películas en {file_path}")
+
+response = requests.get(url_movie_list, headers=headers)
+genres = response.json()["genres"]
+
+print(response.json()["genres"])
+
+file_path = "data/raw/movie_list.json"
+
+with open(file_path, "w", encoding="utf-8") as f:
+    for genre in genres:
+        f.write(json.dumps(genre) + "\n")
+
+print(f"Se guardaron {len(genres)} películas en {file_path}")
