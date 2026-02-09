2715f5bdc5b08aca01b3c53c316cd8c2e5945c69
Sanaz <49277985+sanazamann@users.noreply.github.com>
2026-02-09 12:16:23 +0100

    genres

--- src/download.py
+++ src/download.py
@@ -1,6 +1,4 @@
 import os
-import json
-import requests
 
 from config import ACCESS_TOKEN
 
@@ -11,36 +9,37 @@ headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}"
 }
 
-response = requests.get(url_popular_movies, headers=headers)
-movies = response.json()["results"]
+def api_request():
+    headers = {    
+        "accept": "application/json",
+        "Authorization": f"Bearer {ACCESS_TOKEN}"
+    }
+     
+    response = requests.get(url, headers=headers)
+    return response.json()
 
-os.makedirs("data/raw", exist_ok=True)
 
-#Guardar el archivo
-file_path = "data/raw/popular_movies.json"
-
-try:
+def data_writing():
+     
+    os.makedirs("data/raw", exist_ok=True)
+    
     with open(file_path, "w", encoding="utf-8") as f:
-        for movie in movies:
-            f.write(json.dumps(movie, ensure_ascii=False) + "\n")
+        for element in data:
+            f.write(json.dumps(element) + "\n")
 
-    print(f"Se guardaron {len(movies)} películas en {file_path}")
+    print(f"Se guardaron {len(movies)} elementos en {file_path}")
 
-except Exception as e:
-    print(f"Error al guardar el archivo: {e}")
 
-#Genres
+#movies
+movie_url= "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+movie_data = api_request(movie_url)
+movie_file_path = "data/raw/popular_movies.json"
+data_writing(movie_file_path, movie_data["results"])
 
-url_geners = "https://api.themoviedb.org/3/genre/movie/list"
+#genres
+gene_url = "https://api.theoviedb.org/3/genre/movie/list"
+genre_data = api_request(genre_url)
+genre_file_path = "data/raw/movie_genres.json"
+data_writing(genre_file_path, genre_data["genres"] )
 
-response = requests.get(url_geners, headers=headers)
-genres= response.json()["genres"]
 
-#Guardar el archivo de genre
-file_path= "data/raw/movie_genre.json"
-
-with open(file_path, "w", encoding="utf-8") as f:
-        for genre in genres:
-            f.write(json.dumps(genre) + "\n")
-
-print(f"Se guardaron {len(genres)} géneros en {file_path}")
