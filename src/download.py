5fab9f0a6b39fb6093ccfbd62d42728155a0a7de
jrxschz <cesterin30@icloud.com>
2026-02-09 11:39:46 +0100

    Genre

--- src/download.py
+++ src/download.py
@@ -26,3 +26,24 @@ with open(file_path, "w", encoding="utf-8") as f:
     ## json.dump(movies, f, indent=4, ensure_ascii=False)
 
 print(f"Se guardaron {len(movies)} películas en {file_path}")
+ 
+## Genres
+
+url_genres = "https://api.themoviedb.org/3/genre/movie/list?language=en-US"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {ACCESS_TOKEN}"
+}
+
+response = requests.get(url_genres, headers=headers)
+genres = response.json()["genres"]
+
+# Guardar el archivo
+file_genres = "data/raw/popular_genres.json"
+
+with open(file_genres, "w", encoding="utf-8") as f:
+    for genre in genres:
+        f.write(json.dumps(genre, ensure_ascii=False) + "\n")
+
+print(f"Se guardaron {len(genres)} géneros en {file_genres}")
