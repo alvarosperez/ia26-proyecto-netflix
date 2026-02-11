c095d4ee36ad91e5172b5ff215a576f7cbf4f8b7
Juan Diego <juandisoto29@gmail.com>
2026-02-11 17:07:11 +0100

    popular series

--- src/download.py
+++ src/download.py
@@ -31,8 +31,20 @@ movie_data = api_request(movie_url)
 movie_file_path = "data/raw/popular_movies.json"
 data_writing(movie_file_path, movie_data["results"]) 
 
-# genres
-genre_url = "https://api.themoviedb.org/3/genre/movie/list"
-genre_data = api_request(genre_url)
+# movie genres
+movie_genre_url = "https://api.themoviedb.org/3/genre/movie/list"
+genre_data = api_request(movie_genre_url)
 genre_file_path = "data/raw/movie_genres.json"
 data_writing(genre_file_path, genre_data["genres"])
+
+# series
+series_url = "https://api.themoviedb.org/3/tv/popular"
+series_data = api_request(series_url)
+series_file_path = "data/raw/popular_series.json"
+data_writing(series_file_path, series_data["results"])
+
+# series genres
+series_genre_url = "https://api.themoviedb.org/3/genre/tv/list"
+genre_data = api_request(series_genre_url)
+series_genre_file_path = "data/raw/series_genres.json"
+data_writing(series_genre_file_path, genre_data["genres"])
