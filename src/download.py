9851e61fd247a7514e72c5d53e4fff70a19711c4
PabloOR58 <porellanarevenko@gmail.com>
2026-02-11 18:45:18 +0100

    Actualizacion series

--- src/download.py
+++ src/download.py
@@ -45,3 +45,56 @@ with open(file_path, "w", encoding="utf-8") as f:
         
 
 print(f"Se gaurdaron {len(genres)} películas en {file_path}")
+
+
+def api_request(url):
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {ACCESS_TOKEN}"
+    }
+    response = requests.get(url, headers=headers)
+    # Es buena práctica verificar si la petición fue exitosa
+    response.raise_for_status() 
+    return response.json()
+
+def data_writing(file_path, data):
+    # Aseguramos que la carpeta exista basándonos en la ruta del archivo
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        for element in data:
+            # Escribimos en formato JSONL (JSON Lines) como tenías planeado
+            f.write(json.dumps(element, ensure_ascii=False) + "\n")
+    
+    print(f"Se guardaron {len(data)} elementos en: {file_path}")
+
+
+# 1.  Películas Populares
+movie_url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+movie_data = api_request(movie_url)
+# Corregido el nombre de la variable 'moviw_file_path' -> 'movie_file_path'
+movie_file_path = "data/raw/popular_movies.json"
+data_writing(movie_file_path, movie_data["results"])
+
+# 2.  Géneros
+genre_url = "https://api.themoviedb.org/3/genre/movie/list?language=en-US"
+genre_data = api_request(genre_url)
+genre_file_path = "data/raw/movie_genres.json"
+# Pasamos los argumentos correctos a la función
+data_writing(genre_file_path, genre_data["genres"])
+
+# 3.  Series de TV Populares
+# El endpoint para series es /tv/popular
+tv_url = "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1"
+tv_data = api_request(tv_url)
+tv_file_path = "data/raw/popular_series.json"
+
+# Extraemos 'results' porque el JSON que mostraste tiene la misma estructura que las películas
+data_writing(tv_file_path, tv_data["results"])
+
+# 4. Procesar Géneros de Series 
+tv_genre_url = "https://api.themoviedb.org/3/genre/tv/list?language=en-US"
+tv_genre_data = api_request(tv_genre_url)
+tv_genre_file_path = "data/raw/tv_genres.json"
+
+data_writing(tv_genre_file_path, tv_genre_data["genres"])
