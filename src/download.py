285cf17634564cae04372197e9bd485e817995b9
mateomoliina <moligari55@gmail.com>
2026-02-15 23:00:19 +0100

    Update download.py

--- src/download.py
+++ src/download.py
@@ -35,3 +35,16 @@ url_genres = "https://api.themoviedb.org/3/genre/movie/list?language=en"
 genre_data = api_request(url_genres)
 genres_path = "data/raw/genres.json"
 data_writing(genres_path, genre_data["genres"])   
+
+##Series
+url_series = "https://api.themoviedb.org/3/tv/popular"
+series_data = api_request(url_series)
+series_path = "data/raw/popular_series.json"
+data_writing(series_path, series_data["results"])
+
+##Generes
+
+url_genres_series = "https://api.themoviedb.org/3/genre/tv/list"
+genre_series_data = api_request(url_genres_series)
+genres_series_path = "data/raw/series_genre.json"
+data_writing(genres_series_path, genre_series_data["genres"])
