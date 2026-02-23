c7004c5b132d6c4ee3a74b1a29e07680a5f911eb
Yesesesesesssssssss <166861002+Yesesesesesssssssss@users.noreply.github.com>
2026-02-23 13:20:29 +0100

    Multiple pages for movies and series fixed

--- src/download.py
+++ src/download.py
@@ -15,10 +15,11 @@ def api_request(url):
     return response.json()
 
 
-def data_writing(file_path, data):
+def data_writing(file_path, data, mode = "w"):
 
     os.makedirs("data/raw", exist_ok=True)
-    with open(file_path, mode = "w", encoding="utf-8") as f:
+    
+    with open(file_path, mode, encoding="utf-8") as f:
         for element in data:
             f.write(json.dumps(element, ensure_ascii=False) + "\n")
 
@@ -30,11 +31,10 @@ for pages in range(1,7):
     movie_url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={pages}"
     movie_data = api_request(movie_url)
     movie_file_path = "data/raw/popular_movies.json"
-    data_writing(movie_file_path, movie_data["results"])
     if pages == 1:
-        data_writing(movie_file_path, movie_data, "w")
+        data_writing(movie_file_path, movie_data["results"], "w")
     else:
-        data_writing(movie_file_path, movie_data, "a")
+        data_writing(movie_file_path, movie_data["results"], "a")
 
 #generos 
 genre_url = "https://api.themoviedb.org/3/genre/movie/list?language=en-US"
@@ -42,20 +42,19 @@ genre_data = api_request(genre_url)
 genre_file_path = "data/raw/movie_genres.json"
 data_writing(genre_file_path, genre_data["genres"])
 
+
 #series populares
 for pages in range(1,7):
     tv_url = f"https://api.themoviedb.org/3/tv/popular?language=en-US&page={pages}"
     tv_data = api_request(tv_url)
     tv_file_path = "data/raw/popular_series.json"
-    data_writing(tv_file_path, tv_data["results"])
     if pages == 1:
-        data_writing(tv_file_path, tv_data, "w")
+        data_writing(tv_file_path, tv_data["results"], "w")
     else:
-        data_writing(tv_file_path, tv_data, "a")
+        data_writing(tv_file_path, tv_data["results"], "a")
 
 #generos de series
 tv_genre_url = "https://api.themoviedb.org/3/genre/tv/list?language=en-US"
 tv_genre_data = api_request(tv_genre_url)
 tv_genre_file_path = "data/raw/tv_genres.json"
 data_writing(tv_genre_file_path, tv_genre_data["genres"])
-
