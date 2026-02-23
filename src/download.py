1a1d7e327005caa83a157e39925546a58dcbaf5d
jrxschz <cesterin30@icloud.com>
2026-02-23 09:19:56 +0100

    comit

--- src/download.py
+++ src/download.py
@@ -3,46 +3,6 @@ import requests
 import json
 from config import ACCESS_TOKEN
 
-<<<<<<< HEAD
-def api_request(url):
-    headers = {
-        "accept": "application/json",
-        "Authorization": f"Bearer {ACCESS_TOKEN}"
-    }
-    
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-def data_writing(file_path, data):
-    
-    os.makedirs("data/raw", exist_ok=True)
-
-    with open(file_path, "w", encoding="utf-8") as f:
-        for element in data:
-            f.write(json.dumps(element) + "\n")
-
-    print(f"Se guardaron {len(data)} elementos en {file_path}")
-
-# movies
-movie_url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
-movie_data = api_request(movie_url)
-movie_file_path = "data/raw/popular_movies.json"
-data_writing(movie_file_path, movie_data["results"])
-
-# genres
-genre_url = "https://api.themoviedb.org/3/genre/movie/list"
-genre_data = api_request(genre_url)
-genre_file_path = "data/raw/movie_genres.json"
-data_writing(genre_file_path, genre_data["genres"])
-
-
-# series
-series_url = "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1"
-series_data = api_request(series_url)
-series_file_path = "data/raw/popular_series.json"
-data_writing(series_file_path, series_data["results"])
-=======
-
 def api_request(url):
     headers = {
         "accept": "application/json",
@@ -86,4 +46,5 @@ tv_genre_url = "https://api.themoviedb.org/3/genre/tv/list?language=en-US"
 tv_genre_data = api_request(tv_genre_url)
 tv_genre_file_path = "data/raw/tv_genres.json"
 data_writing(tv_genre_file_path, tv_genre_data["genres"])
->>>>>>> main
+
+
