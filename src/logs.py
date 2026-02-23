c32f0e49c9cef72a18532b74a3279ce409c6aecb
jrxschz <cesterin30@icloud.com>
2026-02-23 11:41:19 +0100

    comit

--- src/logs.py
+++ src/logs.py
@@ -1,4 +1,14 @@
+from datetime import datetime
+
+
 def registro_log(mensaje):
+    ##mensaje con fecha y hora
+    ahora = datetime.now()
+    ## primero imprimo por consola el mensaje
+    print(str(ahora) +  " - " +mensaje)
+    
     ##mensaje se escriba en fichero de texto logs.txt
     with open("data/raw/logs.txt", "a") as fOut:
-        fOut.write(mensaje + "\n")
+        fOut.write(str(ahora) +  " - " +mensaje + "\n")
+        
+        
