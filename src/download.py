57b27c3990ae5347abd8240fafcf30267adc005a
davidcastanos <davidcastanosperez@gmail.com>
2026-02-23 09:51:57 +0100

    Fecha y registros

--- src/download.py
+++ src/download.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 from config import ACCESS_TOKEN
-
+from logs import registro_log
 
 def api_request(url):
     headers = {
@@ -22,7 +22,7 @@ def data_writing(file_path, data):
         for element in data:
             f.write(json.dumps(element, ensure_ascii=False) + "\n")
 
-    print(f"Se guardaron {len(data)} elementos en {file_path}")
+    registro_log(f"Se guardaron {len(data)} elementos en {file_path}")
 
 #peliculas
 movie_url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
