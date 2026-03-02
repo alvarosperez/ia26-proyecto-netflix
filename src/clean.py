d6132dc3cac18d4d4be184acae0902737c8fddde
adrianreigfernandez-oss <reigfernandezadrian@gmail.com>
2026-03-02 10:52:51 +0100

    areglado

--- src/clean.py
+++ src/clean.py
@@ -12,7 +12,10 @@ def crear_diccionario (file_path):
                 cambiar = json.loads(lin.strip())
                
                 diccionario[cambiar.get("id")] = cambiar.get("name")
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         return diccionario
 
 
@@ -31,7 +34,11 @@ def json_to_csv(file_path):
                 fila = {
                     "id": guardar.get("id"),
                     "title": guardar.get("title"),
+<<<<<<< Updated upstream
                     "genre_ids": genres_name,
+=======
+                    "genre_ids": crear_diccionario("data/raw/movie_genres.json"),
+>>>>>>> Stashed changes
                     "popularity": guardar.get("popularity"),
                     "vote_average": guardar.get("vote_average")
                 }
