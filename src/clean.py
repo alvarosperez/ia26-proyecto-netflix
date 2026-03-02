f3c3ab70b3d3ecc3b6385227e422b9a6130c88b3
jrxschz <cesterin30@icloud.com>
2026-03-02 09:11:30 +0100

    comit

--- src/clean.py
+++ src/clean.py
@@ -1,33 +1,4 @@
 import os
-<<<<<<< HEAD
-import json
-import csv
-
-os.makedirs("data/clean", exist_ok=True)
-def clean_movies(input_file, output_file):
-    with open(input_file, "r", encoding="utf-8") as f:
-        movies = json.load(f)
-
-    cleaned_movies = []
-    for movie in movies:
-        cleaned_movie = {
-            "id": movie.get("id"),
-            "title": movie.get("title"),
-            "release_date": movie.get("release_date"),
-            "genre_ids": movie.get("genre_ids"),
-            "vote_average": movie.get("vote_average"),
-            "overview": movie.get("overview")
-        }
-        cleaned_movies.append(cleaned_movie)
-
-    with open(output_file, "w", encoding="utf-8", newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=cleaned_movies[0].keys())
-        writer.writeheader()
-        writer.writerows(cleaned_movies)
-
-
-clean_movies("data/raw/popular_movies.json", "data/clean/popular_movies.csv")
-=======
 import csv
 import json
 
@@ -54,4 +25,3 @@ def json_to_csv(file_path):
         writer.writeheader()
         writer.writerows(out)
 json_to_csv("data/raw/popular_movies.json")
->>>>>>> main
--- src/download.py
+++ src/download.py
@@ -3,27 +3,13 @@ import json
 import requests
 from logs import registro_log
 from config import ACCESS_TOKEN
-<<<<<<< HEAD
-from logs import registro_log
-=======
 from clean import json_to_csv
->>>>>>> main
 
 def api_request(url):
     headers = {
         "accept": "application/json",
         "Authorization": f"Bearer {ACCESS_TOKEN}"
     }
-<<<<<<< HEAD
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-
-def data_writing(file_path, data, mode="w"):
-    os.makedirs("data/raw", exist_ok=True)
-
-    with open(file_path, mode, encoding="utf-8") as f:
-=======
     
     response = requests.get(url, headers=headers)
     return response.json()
@@ -33,47 +19,9 @@ def data_writing(file_path, data,mode="w"):
     os.makedirs("data/raw", exist_ok=True)
 
     with open(file_path, mode,  encoding="utf-8") as f:
->>>>>>> main
         for element in data:
             f.write(json.dumps(element) + "\n")
 
-<<<<<<< HEAD
-    print(f"Se guardaron {len(data)} elementos en {file_path}")
-    registro_log(f"Se guardaron {len(data)} elementos en {file_path}")
-
-
-## peliculas (6 páginas)
-movie_file_path = "data/raw/popular_movies.json"
-for i in range(1, 7):   
-    movie_url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={i}"
-    movie_data = api_request(movie_url)
-    if i == 1:
-        data_writing(movie_file_path, movie_data["results"], mode="w")
-    else:   
-        data_writing(movie_file_path, movie_data["results"], mode="a")
-
-
-## generos de peliculas
-genre_url = "https://api.themoviedb.org/3/genre/movie/list?language=en-US"
-genre_data = api_request(genre_url)
-genre_file_path = "data/raw/movie_genres.json"
-data_writing(genre_file_path, genre_data["genres"], mode="w")
-
-
-## series populares 
-tv_url = "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1"
-tv_data = api_request(tv_url)
-tv_file_path = "data/raw/popular_series.json"
-data_writing(tv_file_path, tv_data["results"], mode="w")
-
-
-## generos de series
-tv_genre_url = "https://api.themoviedb.org/3/genre/tv/list?language=en-US"
-tv_genre_data = api_request(tv_genre_url)
-tv_genre_file_path = "data/raw/tv_genres.json"
-data_writing(tv_genre_file_path, tv_genre_data["genres"], mode="w")
-
-=======
         #print(f"Se guardaron {len(data)} elementos en {file_path}")
         registro_log(f"Se guardaron {len(data)} elementos en {file_path}")
 
@@ -111,4 +59,3 @@ data_writing(generos_file_path, genre_data["genres"])
 
 
     
->>>>>>> main
