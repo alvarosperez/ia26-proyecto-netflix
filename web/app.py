37594250463dfa8feb3cd889c0ee7f00e9f9975a
jrxschz <cesterin30@icloud.com>
2026-03-24 19:52:29 +0100

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,10 +1,35 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import os
 
 st.title("Proyecto Netflix")
-st.subheader("Resumen - Datos")
 
-# Ruta correcta al CSV
-movies_df = pd.read_csv("data/popular_movies.csv")   
+st.subheader("Resumen - datos")
 
-st.write(movies_df.head(3))
+# 🔧 ARREGLO DE RUTA
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+csv_path = os.path.join(base_dir, "data", "clean", "popular_movies.csv")
+
+df = pd.read_csv(csv_path)
+
+st.write(df.head(3))
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Número de películas", df["title"].count())
+
+col2.metric("Valoración media", df["vote_average"].mean())
+
+import matplotlib.pyplot as plt
+import ast
+
+generos = df["genre_ids"] \
+    .apply(ast.literal_eval).explode().value_counts() \
+    .reset_index()
+
+st.write(generos.head(2))
+
+fig, ax = plt.subplots()
+ax.barh(generos["genre_ids"], generos["count"])
+
+st.pyplot(fig)
