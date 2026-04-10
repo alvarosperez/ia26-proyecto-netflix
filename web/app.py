64b285f0428d2ada1e1aa9bd14a7c8d298f47b2b
adrianreigfernandez-oss <reigfernandezadrian@gmail.com>
2026-04-10 09:38:05 +0200

    cambio

--- web/app.py
+++ web/app.py
@@ -1,1 +1,28 @@
+import pandas as pd
 import streamlit as st
+
+st.title("Proyecto Netflix")
+
+st.subheader("Resumen - Datos")
+
+df = pd.read_csv("./data/clean/popular_movies.csv")
+
+st.write (df.head(3))
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Numero de peliculas", df["title"].count())
+
+col2.metric("Valoracion media", df["vote_average"].mean())
+
+import matplotlib.pyplot as plt
+import ast
+
+generos = df["genre_ids"] \
+    .apply(ast.literal_eval).explode().value_counts().reset_index()
+
+st.write(generos.head(2))
+
+fig, ax = plt.subplots()
+ax = plt.barh(generos["genre_ids"], generos["count"])
+st.pyplot(fig)
